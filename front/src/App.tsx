6668ba01f139430ffad7515328bb52c88826fd29
Eugrn Guriev <lfeedwork05@gmail.com>
2024-04-02 19:31:17 +0200

    initialized layout components

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 const App: React.FC = () => {
-  return (
-    <div className="App"></div>
-  );
-}
+  return <div></div>;
+};
 
 export default App;
---
+++ front/src/components/common/rombic-button/rombic-button.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const RombicButton: React.FC = () => {
+  return (
+    <div>RombicBtn</div>
+  )
+}
