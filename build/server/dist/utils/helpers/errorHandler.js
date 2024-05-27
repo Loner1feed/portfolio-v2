4ca3a688160d653495c9480457a36d1ff4fdb0be
Eugrn Guriev <lfeedwork05@gmail.com>
2024-05-27 21:05:33 +0200

    removed build folder

--- build/server/dist/utils/helpers/errorHandler.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = void 0;
-const errorHandler = (error) => {
-    console.log("\x1b[33mServer error:\x1b[0m " + error.message);
-};
-exports.errorHandler = errorHandler;
